38601adb2204df4d676a6209dcd440c82fe0c10e
Mu'tasim M <43227513+muhtasimMutasim@users.noreply.github.com>
2018-11-26 19:38:36 -0500

    Update StockBackEnd2.py

--- StockBackEnd2.py
+++ StockBackEnd2.py
@@ -1,18 +1,18 @@
 import os
 import urllib
-from StockBackend import opening as op
-from StockBackend import returnRate as rr
-from StockBackend import pc_data as pd
+from StockBackEnd import opening as op
+from StockBackEnd import returnRate as rr
+from StockBackEnd import pc_data as pd
 
 
 def sendMessage(mess):
-    groupMeID = ''#insert Group Me ID
+    groupMeID = str('')#insert Group Me ID
     m = urllib.parse.quote(mess, safe='_.-~')
-    command = 'curl -X POST \"https://api.groupme.com/v3/bots/post?bot_id=%s&text=' + m + '\"' % groupMeID
+    command = 'curl -X POST \"https://api.groupme.com/v3/bots/post?bot_id='+ groupMeID +'&text=' + m + '\"'
     return(os.system(command))
 
 if __name__ == '__main__':
     #for i in reList(opening()):# outputs the groupme messages
-    for i in rr(pc_Data(op(230))):
+    for i in rr(pd(op(23))):
         sendMessage(i)
         print(i)
