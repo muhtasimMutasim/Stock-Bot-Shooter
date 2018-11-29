c33fa84e216919ce5b483eec262ca9b85fff1f9d
Mu'tasim M <43227513+muhtasimMutasim@users.noreply.github.com>
2018-11-28 21:00:33 -0500

    Update StockBackEnd2.py

--- StockBackEnd2.py
+++ StockBackEnd2.py
@@ -1,18 +1,40 @@
+import time
 import os
+import subprocess
 import urllib
+import requests as r
 from StockBackEnd import opening as op
 from StockBackEnd import returnRate as rr
 from StockBackEnd import pc_data as pd
+from StockBackEnd import sbeTime as t
+start_time = time.time()
 
-
-def sendMessage(mess):
+#this function gives the option of request posting to the url
+def smRequestPost(mess):
+    response = r.get("https://api.groupme.com/v3/bots/post")
     groupMeID = str('')#insert Group Me ID
-    m = urllib.parse.quote(mess, safe='_.-~')
+    p_params = { 'bot_id' : groupMeID, 'text': str(mess)}
+    message = r.post("https://api.groupme.com/v3/bots/post", params = p_params)
+    if (response.status_code == 200):
+        return(message)
+    else:
+        time.sleep(1)
+        return(message)
+#this function gives the option of subprocessing or execution through the terminal
+def smSubProcces(mess):
+    groupMeID = str('') #insert Group Me ID
+    m = urllib.parse.quote_plus(mess, safe='_.-~')
     command = 'curl -X POST \"https://api.groupme.com/v3/bots/post?bot_id='+ groupMeID +'&text=' + m + '\"'
-    return(os.system(command))
+    return(subprocess.run(command))
 
 if __name__ == '__main__':
-    #for i in reList(opening()):# outputs the groupme messages
-    for i in rr(pd(op(230))):
-        sendMessage(i)
-        print(i)
+    # outputs the groupme messages
+    #x = rr(pd(op(499)))
+    a = 1
+    for i in rr(pd(op(100))):
+        #smRequestPost(i)
+        smSubProcces(i)
+        print(a, i)
+        a += 1
+    t() # this function and the statement below measures and prints the execution time for both files
+    print("---StockBackEnd2 took: %s seconds --- # of Companies: %s" % ((time.time() - start_time), (a-1)))
